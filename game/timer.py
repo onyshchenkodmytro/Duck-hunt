3b19a960b6d746952ce6b6fc96ec56f4084f268e
onyshchenkodmytro <dimonchakus@gmail.com>
2025-04-29 21:07:50 +0300

    Add Timer class for round duration

---
+++ game/timer.py
@@ -0,0 +1,15 @@
+import time
+
+class Timer:
+    def __init__(self, duration):
+        self.duration = duration
+        self.start_time = time.time()
+
+    def reset(self):
+        self.start_time = time.time()
+
+    def is_time_up(self):
+        return (time.time() - self.start_time) > self.duration
+
+    def get_time_left(self):
+        return max(0, self.duration - (time.time() - self.start_time))
