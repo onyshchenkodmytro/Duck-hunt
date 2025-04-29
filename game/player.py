50d49445cde9f62a3795ee8b11c790e48c82d248
onyshchenkodmytro <dimonchakus@gmail.com>
2025-04-29 21:06:57 +0300

    Add Player class with shooting and lives logic

---
+++ game/player.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Player:
+    def __init__(self):
+        self.bullets = 3
+        self.lives = 5
+        self.score = 0
+
+    def shoot(self):
+        if self.bullets > 0:
+            self.bullets -= 1
+            return True
+        return False
+
+    def reload(self):
+        self.bullets = 3
+
+    def lose_life(self, count=1):
+        self.lives -= count
+
+    def is_alive(self):
+        return self.lives > 0
